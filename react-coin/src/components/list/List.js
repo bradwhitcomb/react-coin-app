69c8c5162086a0405c4f6087e183d3bbaa50d16b
JBW <whitco670@jbw-mbp-215.attlocal.net>
2018-04-19 15:14:33 -0400

    table formats correctly.  page/loading pagination works.

--- react-coin/src/components/list/List.js
+++ react-coin/src/components/list/List.js
@@ -1,54 +1,112 @@
 import React from 'react';
-import {handleResponse} from '../../helpers';
+import { handleResponse } from '../../helpers';
 import {API_URL} from '../../config';
+import Loading from '../common/Loading';
+import Table from './Table';
+import Pagination from './Pagination';
+
 
 
 class List extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            loading: false,
-            currencies: [],
-            error: null,
-        };
-    }
+    this.state = {
+      loading: false,
+      currencies: [],
+      error: null,
+      totalPages: 0,
+      page: 1,
+    };
 
-    componentDidMount() {
-        this.setState({ loading: true });
-        fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
-        .then(handleResponse)
-        .then((data) => {
-            
-          
-          this.setState({
-              currencies:data.currencies, 
-              loading:false,
-            });
-        })
-        .catch((error) => {
-            this.setState({
-                error:error.errorMessage, 
-                loading:false,
-            });
-          
+    this.handlePaginationClick = this.handlePaginationClick.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCurrencies();
+    
+  }
+
+  fetchCurrencies() {
+    this.setState({ loading: true });
+
+    const { page } = this.state;
+
+    fetch(`${API_URL}/cryptocurrencies?page=${page}&perPage=20`)
+      .then(handleResponse)
+      .then((data) => {
+        const{ currencies, totalPages } = data;
+        this.setState({
+          currencies,
+          totalPages,
+          loading: false,
         });
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.errorMessage,
+          loading: false,
+        });
+      });
 
-    }
-    render() {
-        console.log(this.state);
-        if (this.state.loading){
-            return <div>Loading....</div>
-        }
-        return (
-            <div>
-            {this.state.currencies.map((currency) => (
-                <div key={currency.id}>{currency.id}</div>
-            ))}
+  }
 
-            </div>
-        );
+  renderChangePercent(percent){
+    if (percent > 0){
+      return <span className="percent-raised">{percent}% &uarr;</span>
+    } else if (percent < 0) {
+      return <span className="percent-fallen">{percent}% &uarr;</span>
+    } else {
+      return <span>{percent}</span>
     }
+
+  }
+
+handlePaginationClick(direction) {
+
+
+  let nextPage = this.state.page;
+
+  //Increment next page if direction varialbe is next otherwise decrement next page
+  nextPage = direction==="next" ? nextPage +1 : nextPage -1;
+
+  this.setState({page: nextPage}, () => {
+    //call fetchCurrencies function inside setSates's callback
+    //because we have to insure first page state is updated before successive pages
+    this.fetchCurrencies();
+  });
+}
+
+
+
+  render() {
+    const {loading, error, currencies, page , totalPages} = this.state;
+    
+    // render only loading component, if loading state is set to true
+    if (loading) {
+      return <div className='loading-container'><Loading/></div>
+    }
+
+    // render only error messge, if error occurredd while fetching data
+    if (error) {
+      return <div className="error">{error}</div>
+    }
+    return (
+      <div>
+       <Table 
+          currencies ={currencies}
+          renderChangePercent={this.renderChangePercent}
+        />
+
+        <Pagination
+          page={page}
+          totalPages={totalPages}
+          handlePaginationClick={this.handlePaginationClick}
+        />
+       </div> 
+     
+    );
+  }
 }
 
 export default List;
